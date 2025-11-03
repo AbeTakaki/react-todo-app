a0339926493d60f9479a2d76111c465349338634
AbeTakaki <abetakaki0101@gmail.com>
2025-11-03 19:32:36 +0900

    ダミーデータの作成

---
+++ src/data/dummyTask.js
@@ -0,0 +1,17 @@
+export const dummyTask = [
+  {
+    id: 1,
+    title: "reactの勉強",
+    status: "notStarted",
+  },
+  {
+    id: 2,
+    title: "夕食を作る",
+    status: "completed",
+  },
+  {
+    id: 3,
+    title: "部屋を掃除する",
+    status: "trashed",
+  },
+];
