893b2d1b87f2deb408a94526aba1d60107efb669
AbeTakaki <abetakaki0101@gmail.com>
2025-11-04 22:17:05 +0900

    更新処理を追加

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -11,7 +11,7 @@ const inputVariants = cva("flex-1 border px-2 py-1 border-gray-300 bg-white", {
   },
 });
 
-export function TaskItem({ task }) {
+export function TaskItem({ task, onChange }) {
   return (
     <div className="flex items-center gap-3 rounded bg-white px-4 py-2">
       <div className="flex items-center">
@@ -20,6 +20,12 @@ export function TaskItem({ task }) {
           type="checkbox"
           className="size-5 cursor-pointer"
           checked={task.status === "completed"}
+          onChange={(e) => 
+            onChange(task.id, {
+              // チェック状態に基づいてタスクのステータスを更新
+              status: e.target.checked ? "completed" : "notStarted",
+            })
+          }
         />
       </div>
       <input 
@@ -27,11 +33,29 @@ export function TaskItem({ task }) {
         className={inputVariants({ completed: task.status === "completed" })}
         defaultValue={task.title}
         disabled={task.status === "completed"}
+        onKeyDown={(event) => {
+          // Enterキーで編集を確定し、入力フィールドからフォーカスを外す
+          if (event.nativeEvent.isComposing || event.key !== "Enter") {
+            return;
+          }
+          event.currentTarget.blur();
+        }}
+        inBlur={(e) => {
+          onChange(task.id, {
+            // フォーカスが外れたタイミングでタスクのタイトルを更新
+            title: e.target.value,
+          });
+        }}
       />
       {/* タスクをゴミ箱に移動するボタン */}
       <button
         type="button"
         className="rounded bg-gray-200 p-2 transition-colors hover:bg-gray-300"
+        onClick={() => 
+          onChange(task.id, {
+            status: "trashed",
+          })
+        }
         aria-label={`タスク 「${task.title}」をゴミ箱へ移動する`}
       >
         <Trash2 className="size-5 text-gray-500" />
